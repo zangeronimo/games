fda7968e158d773cbd8b2f51934a373f95aef0f9
Luciano Zang <zangeronimo@gmail.com>
2021-05-16 08:26:08 -0300

    Hash game with dificult lvl mode

--- src/pages/Hash/index.tsx
+++ src/pages/Hash/index.tsx
@@ -22,6 +22,7 @@ export const Hash: React.FC = () => {
   const [winner, setWinner] = useState(false);
   const [win, setWin] = useState<boolean>(false);
   const [older, setOlder] = useState(false);
+  const [mode, setMode] = useState(1);
   const [pieces, setPieces] = useState<PieceData[]>(
     new Array(9)
       .fill(null)
@@ -98,7 +99,9 @@ export const Hash: React.FC = () => {
     [pieces]
   );
 
-  const process = useCallback(() => {
+  const mode1 = useCallback(() => {
+    if (mode === 0) return false;
+
     const win1 = hasPossibilityToWin(0, 1, 2);
     const win2 = hasPossibilityToWin(0, 3, 6);
     const win3 = hasPossibilityToWin(0, 4, 8);
@@ -125,39 +128,79 @@ export const Hash: React.FC = () => {
     } else if (win8) {
       handleClick(pieces[win8]?.label);
     } else {
-      const lose1 = hasPossibilityToLose(0, 1, 2);
-      const lose2 = hasPossibilityToLose(0, 3, 6);
-      const lose3 = hasPossibilityToLose(0, 4, 8);
-      const lose4 = hasPossibilityToLose(1, 4, 7);
-      const lose5 = hasPossibilityToLose(2, 5, 8);
-      const lose6 = hasPossibilityToLose(2, 4, 6);
-      const lose7 = hasPossibilityToLose(3, 4, 5);
-      const lose8 = hasPossibilityToLose(6, 7, 8);
+      return false;
+    }
 
-      if (lose1 || lose1 === 0) {
-        handleClick(pieces[lose1]?.label);
-      } else if (lose2 || lose2 === 0) {
-        handleClick(pieces[lose2]?.label);
-      } else if (lose3 || lose3 === 0) {
-        handleClick(pieces[lose3]?.label);
-      } else if (lose4) {
-        handleClick(pieces[lose4]?.label);
-      } else if (lose5) {
-        handleClick(pieces[lose5]?.label);
-      } else if (lose6) {
-        handleClick(pieces[lose6]?.label);
-      } else if (lose7) {
-        handleClick(pieces[lose7]?.label);
-      } else if (lose8) {
-        handleClick(pieces[lose8]?.label);
-      } else {
-        const free = pieces.filter((item) => item.icon === undefined);
-        if (free.length === 0) setOlder(true);
-        const selected = Math.floor(Math.random() * free.length);
-        handleClick(free[selected]?.label);
+    return true;
+  }, [handleClick, hasPossibilityToWin, pieces, mode]);
+
+  const mode2 = useCallback(() => {
+    if (mode < 2) return false;
+
+    const lose1 = hasPossibilityToLose(0, 1, 2);
+    const lose2 = hasPossibilityToLose(0, 3, 6);
+    const lose3 = hasPossibilityToLose(0, 4, 8);
+    const lose4 = hasPossibilityToLose(1, 4, 7);
+    const lose5 = hasPossibilityToLose(2, 5, 8);
+    const lose6 = hasPossibilityToLose(2, 4, 6);
+    const lose7 = hasPossibilityToLose(3, 4, 5);
+    const lose8 = hasPossibilityToLose(6, 7, 8);
+
+    if (lose1 || lose1 === 0) {
+      handleClick(pieces[lose1]?.label);
+    } else if (lose2 || lose2 === 0) {
+      handleClick(pieces[lose2]?.label);
+    } else if (lose3 || lose3 === 0) {
+      handleClick(pieces[lose3]?.label);
+    } else if (lose4) {
+      handleClick(pieces[lose4]?.label);
+    } else if (lose5) {
+      handleClick(pieces[lose5]?.label);
+    } else if (lose6) {
+      handleClick(pieces[lose6]?.label);
+    } else if (lose7) {
+      handleClick(pieces[lose7]?.label);
+    } else if (lose8) {
+      handleClick(pieces[lose8]?.label);
+    } else {
+      return false;
+    }
+
+    return true;
+  }, [handleClick, hasPossibilityToLose, pieces, mode]);
+
+  const mode3 = useCallback(() => {
+    if (mode < 3) return false;
+
+    if (pieces[4].icon === undefined) {
+      handleClick(pieces[4].label);
+    } else if (pieces[0].icon === undefined) {
+      handleClick(pieces[0].label);
+    } else if (pieces[2].icon === undefined) {
+      handleClick(pieces[2].label);
+    } else if (pieces[6].icon === undefined) {
+      handleClick(pieces[6].label);
+    } else if (pieces[8].icon === undefined) {
+      handleClick(pieces[8].label);
+    } else {
+      return false;
+    }
+
+    return true;
+  }, [handleClick, pieces, mode]);
+
+  const process = useCallback(() => {
+    if (!mode1()) {
+      if (!mode2()) {
+        if (!mode3()) {
+          const free = pieces.filter((item) => item.icon === undefined);
+          if (free.length === 0) setOlder(true);
+          const selected = Math.floor(Math.random() * free.length);
+          handleClick(free[selected]?.label);
+        }
       }
     }
-  }, [handleClick, pieces, hasPossibilityToWin, hasPossibilityToLose]);
+  }, [handleClick, pieces, mode1, mode2, mode3]);
 
   const checkWinner = useCallback(
     (start: number, pos1: number, pos2: number) => {
@@ -209,19 +252,28 @@ export const Hash: React.FC = () => {
           <FaToiletPaper size={28} />
         </button>
 
-        {!winner && toggle && <Spinner />}
+        <div>
+          {!winner && toggle && <Spinner />}
 
-        {winner && (
-          <p>
-            {win ? (
-              <FaSmileBeam style={{ color: "#0f0" }} size={48} />
-            ) : (
-              <FaSadCry style={{ color: "#f00" }} size={48} />
-            )}
-          </p>
-        )}
+          {winner && (
+            <p>
+              {win ? (
+                <FaSmileBeam style={{ color: "#0f0" }} size={48} />
+              ) : (
+                <FaSadCry style={{ color: "#f00" }} size={48} />
+              )}
+            </p>
+          )}
 
-        {older && !winner && <FaPoo style={{ color: "#434304" }} size={48} />}
+          {older && !winner && <FaPoo style={{ color: "#434304" }} size={48} />}
+        </div>
+
+        <select value={mode} onChange={(e) => setMode(Number(e.target.value))}>
+          <option value={0}>Fácil</option>
+          <option value={1}>Médio</option>
+          <option value={2}>Desafio</option>
+          <option value={3}>Geminiano</option>
+        </select>
       </Title>
       <Board>
         {pieces &&
